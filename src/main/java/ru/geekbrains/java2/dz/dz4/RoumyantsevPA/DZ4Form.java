4c1242314da5bcb939b0fc35ed47a8189f3b4e11
Naif <knifffe@yandex.ru>
2017-11-28 16:42:25 +0300

    дз4

---
+++ src/main/java/ru/geekbrains/java2/dz/dz4/RoumyantsevPA/DZ4Form.java
@@ -0,0 +1,66 @@
+package ru.geekbrains.java2.dz.dz4.RoumyantsevPA;
+
+import javax.swing.*;
+import java.util.Date;
+
+public class DZ4Form extends JFrame {
+    private JPanel panelka;
+    private JTextField textField1;
+    private JButton button1;
+    private JEditorPane editorPane1;
+    private JPanel panelka2;
+    private JScrollPane scrol1;
+    private JList list1;
+
+    public DZ4Form() {
+        setSize(800, 600);
+        setTitle("][ack the planet. . .");
+        setContentPane(panelka);
+        setVisible(true);
+        textField1.setBorder(null);
+        button1.setBorder(null);
+        scrol1.setHorizontalScrollBarPolicy(31);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        String text1 = "Wake up, Neo. . .                     ";
+        String text2 = "The Matrix has you. . .               ";
+        String text3 = "Follow the white rabbit.              ";
+        String text4 = "Knock, Knock, Neo.                    ";
+        String text = text1 + text2 + text3 + text4;
+        String runText = "";
+        for (int i = 0; i < text.length(); i++) {
+            if (i == text1.length() ||
+                    i == text2.length() + text1.length() ||
+                    i == text3.length() + text2.length() + text1.length() ||
+                    i == text4.length() + text3.length() + text2.length() + text1.length()) {
+                runText = "";
+            }
+            runText += text.charAt(i);
+            editorPane1.setText(runText);
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        editorPane1.setText("");
+
+        button1.addActionListener(
+                e -> {
+                    editorPane1.setText(editorPane1.getText() + "\nYou:" + new Date( )+"\n" + textField1.getText());
+                    textField1.setText("");
+                     });
+
+        textField1.addActionListener(
+                e -> {
+                    editorPane1.setText(editorPane1.getText() + "\nYou:" + new Date()+"\n"+ textField1.getText());
+                    textField1.setText("");
+                     }
+        );
+    }
+
+    public static void main(String[] args) {
+        new DZ4Form();
+    }
+}
+
--- src/main/java/ru/geekbrains/java2/lesson4/p2/Form2.java
+++ src/main/java/ru/geekbrains/java2/lesson4/p2/Form2.java
@@ -4,20 +4,33 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+//import static com.sun.glass.ui.Cursor.setVisible;
+
 /**
  * Created by i on 27.11.2017.
  */
-public class Form2 {
-    private JButton buttonкурнеButton;
+public class Form2 extends JFrame{
+    private JButton Кнопка;
     private JTable table1;
     private JTabbedPane tabbedPane1;
+    private JPanel ttttt;
+
 
     public Form2() {
-        buttonкурнеButton.addActionListener(new ActionListener() {
+        setSize(800,600);
+        setContentPane(ttttt);
+        setVisible(true);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        Кнопка.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
 
             }
         });
+    }
+
+    public static void main(String[] args) {
+        new Form2();
     }
 }
