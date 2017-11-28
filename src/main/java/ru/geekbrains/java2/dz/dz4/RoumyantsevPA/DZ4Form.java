9b23556059e230b91df8603c1a1d2ea01b3753eb
Naif <knifffe@yandex.ru>
2017-11-28 23:46:15 +0300

    Иконка

--- src/main/java/ru/geekbrains/java2/dz/dz4/RoumyantsevPA/DZ4Form.java
+++ src/main/java/ru/geekbrains/java2/dz/dz4/RoumyantsevPA/DZ4Form.java
@@ -1,6 +1,9 @@
 package ru.geekbrains.java2.dz.dz4.RoumyantsevPA;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -14,8 +17,10 @@ public class DZ4Form extends JFrame {
     private JList list1;
 
     public DZ4Form() {
+        Image img = Toolkit.getDefaultToolkit().getImage(".\\src\\main\\java\\ru\\geekbrains\\java2\\dz\\dz4\\RoumyantsevPA\\java.png");
+        setIconImage(img);
         setSize(800, 600);
-        setTitle("][ack the planet. . .");
+        setTitle("M a t r i x   has   U. . .");
         setContentPane(panelka);
         setVisible(true);
         textField1.setBorder(null);
@@ -28,37 +33,38 @@ public class DZ4Form extends JFrame {
         String text3 = "Follow the white rabbit.              ";
         String text4 = "Knock, Knock, Neo.                    ";
         String text = text1 + text2 + text3 + text4;
-        String runText = "";
+        //        SimpleDateFormat formatForDateNow = new SimpleDateFormat("E yyyy.MM.dd 'и время' hh:mm:ss a zzz");
+        SimpleDateFormat formatForDateNow = new SimpleDateFormat("(k:mm:ss dd/MM/yyyy)");
+        String runText = "unknown " + formatForDateNow.format(new Date()) + "\n";
         for (int i = 0; i < text.length(); i++) {
             if (i == text1.length() ||
                     i == text2.length() + text1.length() ||
                     i == text3.length() + text2.length() + text1.length() ||
                     i == text4.length() + text3.length() + text2.length() + text1.length()) {
-                runText = "";
+                runText += "\nunknown " + formatForDateNow.format(new Date()) + "\n";
             }
             runText += text.charAt(i);
             editorPane1.setText(runText);
             try {
-                Thread.sleep(50);
+                Thread.sleep(130);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
-        editorPane1.setText("");
-//        SimpleDateFormat formatForDateNow = new SimpleDateFormat("E yyyy.MM.dd 'и время' hh:mm:ss a zzz");
-        SimpleDateFormat formatForDateNow = new SimpleDateFormat("(k:mm:ss dd/MM/yyyy)");
+        //editorPane1.setText("");
+
 
         button1.addActionListener(
                 e -> {
-                    editorPane1.setText(editorPane1.getText() + "\nYou " + formatForDateNow.format( new Date())+" :\n" + textField1.getText());
+                    editorPane1.setText(editorPane1.getText() + "\nYou " + formatForDateNow.format(new Date()) + "\n" + textField1.getText());
                     textField1.setText("");
-                     });
+                });
 
         textField1.addActionListener(
                 e -> {
-                    editorPane1.setText(editorPane1.getText() + "\nYou " + formatForDateNow.format( new Date())+" :\n"+ textField1.getText());
+                    editorPane1.setText(editorPane1.getText() + "\nYou " + formatForDateNow.format(new Date()) + "\n" + textField1.getText());
                     textField1.setText("");
-                     }
+                }
         );
     }
 
